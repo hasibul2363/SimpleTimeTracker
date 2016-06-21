5ae6000af9e22153cc83ddd763c7f540e8c3d2cf
hasibul2363 <hasibul2363@gmail.com>
2016-06-21 10:04:11 +0000

    my initial commit

---
+++ Infrastructure/ServerSettingProvider/ServerSettingProvider/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSettingProvider
+{
+    public class Class1
+    {
+        //This is my first changes
+    }
+}
