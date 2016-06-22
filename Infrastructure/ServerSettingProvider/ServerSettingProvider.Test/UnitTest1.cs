14eea3859ccd78ede4a7d34e8b6850e990048990
hasibul2363 <hasibul2363@gmail.com>
2016-06-22 09:29:10 +0000

    Added test project

---
+++ Infrastructure/ServerSettingProvider/ServerSettingProvider.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerSettingProvider;
+
+namespace ServerSettingProvider.Test
+{
+    [TestClass]
+    public class ServerSettingsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Guid.Parse("081b8162-8a2a-4a34-8c28-1c081b6768ee"), ServerSettings.GetSystemUserId());
+        }
+    }
+}
---
+++ Infrastructure/ServerSettingProvider/ServerSettingProvider/ServerSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSettingProvider
+{
+    public class ServerSettings
+    {
+        public static Guid GetSystemUserId()
+        {
+            return Guid.Parse("081b8162-8a2a-4a34-8c28-1c081b6768ee");
+        }
+    }
+}
